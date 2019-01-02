d5416e16a1582fd8e8da78d971832f7a6c91b50f
Mdmin <1490268185@qq.com>
2019-01-02 11:48:28 +0800

    qq

---
+++ WMS/Controllers/WCSController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMS.Controllers
+{
+    public class WCSController : Controller
+    {
+        // GET: WCS
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region 仓库实时运行状况
+        public ActionResult WarehouseState()
+        {
+
+            return View();
+        }
+        #endregion
+        #region 监控台
+        public ActionResult Console() {
+
+            return View();
+        }
+        #endregion
+        #region 命令分解
+        public ActionResult Command() {
+            return View();
+        }
+        #endregion
+        #region 机器信息注册
+        public ActionResult Machine()
+        {
+            return View();
+        }
+        #endregion
+        #region 功能位置信息注册
+        public ActionResult Functional()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ WMS/Controllers/WMSController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMS.Controllers
+{
+    public class WMSController : Controller
+    {
+        // GET: WMS
+        public ActionResult Index()
+        {
+            return View();
+        }
+        #region 入库
+        public ActionResult Enter()
+        {
+
+            return View();
+        }
+        #endregion
+        #region 出库
+        public ActionResult Out()
+        {
+
+            return View();
+        }
+        #endregion
+        #region 库位
+        public ActionResult WarehousePosition()
+        {
+            return View();
+        }
+        #endregion
+        #region 库区
+        public ActionResult WarehouseArea()
+        {
+            return View();
+        }
+        #endregion
+        #region 仓库
+        public ActionResult Warehouse()
+        {
+            return View();
+        }
+        #endregion
+        #region 货位
+        public ActionResult GoodsArea()
+        {
+            return View();
+        }
+        #endregion
+        #region 物料信息
+        public ActionResult Material()
+        {
+            return View();
+        }
+        #endregion
+        #region 库存限制维护
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+        #endregion
+        #region 策略
+        public ActionResult Strategy()
+        {
+            return View();
+        }
+        #endregion
+        #region 员工登陆
+        public ActionResult login()
+        {
+            return View();
+        }
+        #endregion
+    }
+}

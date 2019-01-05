4f4f01512d05e2f0030807b243665fe2c559d0d1
Mdmin <1490268185@qq.com>
2019-01-05 17:39:05 +0800

    12

--- WMS/Controllers/HomeController.cs
+++ WMS/Controllers/HomeController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections;
 using System.Web.Mvc;
+using WMS.Models;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace WMS.Controllers
 {
@@ -10,6 +14,68 @@ namespace WMS.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult Registered() {
+
+            return View();
+        }
+        //注册-更新
+        public string AddUser(Dictionary<string,string> User,string type) {
+            using (WMSEntities ms =new WMSEntities ()) {
+                SqlParameter[] parameters = new SqlParameter[User.Count()];
+                StringBuilder builder = new StringBuilder("exec AddUser @type="+type+",");
+                int i = 0;
+                foreach (var user in User) {
+                    parameters[i] = new SqlParameter("@"+user.Key,user.Value);
+                    if ((i + 1) == User.Count())
+                    {
+                        builder.Append("@" + user.Key + "=@" + user.Key);
+                    }
+                    else {
+                        builder.Append("@" + user.Key + "=@" + user.Key+",");
+                    }
+                    i++;
+                }
+                ms.Database.ExecuteSqlCommand(builder.ToString(),parameters);
+            }
+                return "true";
+        }
+        //登陆
+        public string Login(string Name,string Pwd, bool WCS) {
+            List<Employee> employees;
+            using (WMSEntities mSEntities=new WMSEntities()) {
+                employees = mSEntities.Employee.Where(p => p.PWD == Pwd).Where(p => p.UserName == Name).ToList();
+                if (employees.Count() > 0)
+                {
+                    Session["user"] = employees[0].UserName;
+                    Session["WCS"] = WCS;
+                    return "true";
+                }
+                else {
+                    return "false";
+                }
+            }
+          
+        }
+        //注销
+        public string OutUser() {
+            Session.RemoveAll();
+            return "true";
+        }
+        //查询用户
+        public ActionResult checkUser(string value) {
+            List<Employee> employees;
+            
+            using (WMSEntities wm=new WMSEntities()) {
+
+                    employees = wm.Employee.Where(p => p.UserName == value).ToList();
+
+            }
+            return Json(employees,JsonRequestBehavior.AllowGet);
+        }
+        //基本资料
+        public ActionResult UserData() {
             return View();
         }
     }
--- WMS/Controllers/WMSController.cs
+++ WMS/Controllers/WMSController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Collections;
 using System.Web.Mvc;
+using WMS.Models;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace WMS.Controllers
 {
@@ -50,30 +54,333 @@ namespace WMS.Controllers
         {
             return View();
         }
-        #endregion
-        #region 物料信息
-        public ActionResult Material()
+        //添加货位仓库等
+        public ActionResult AddAll()
         {
+
             return View();
         }
-        #endregion
-        #region 库存限制维护
-        public ActionResult Inventory()
+        //添加仓库等
+        public string AddWarehouse(Dictionary<string, string> data, string type)
         {
-            return View();
+            StringBuilder builder = new StringBuilder("exec AddStorage @type=" + type + ",@CreatedBy=11,");
+            using (WMSEntities ws = new WMSEntities())
+            {
+                int index = 0;
+                int len = data.Count();
+                SqlParameter[] parameter = new SqlParameter[len];
+                foreach (var add in data)
+                {
+                    parameter[index] = new SqlParameter("@" + add.Key, add.Value);
+                    builder.Append("@" + add.Key + "=@" + add.Key + ",");
+                    index++;
+                }
+                string sql = builder.ToString().Substring(0, builder.ToString().LastIndexOf(","));
+                ws.Database.ExecuteSqlCommand(sql, parameter);
+            }
+            return "true";
         }
-        #endregion
-        #region 策略
-        public ActionResult Strategy()
+        //查询仓库信息
+        public ActionResult checkWo(string id, string value, string data)
         {
-            return View();
+           
+          
+            using (WMSEntities wMS = new WMSEntities())
+            {
+
+                if (id == "1")
+                {
+                    if (data == null)
+                    {
+                        data = "";
+                    }
+                    SqlParameter[] parameters = new SqlParameter[1];
+                    parameters[0] = new SqlParameter("@id", data);
+                    List<checkWo_Result> list1 = wMS.Database.SqlQuery<checkWo_Result>("exec checkWo @id", parameters).ToList();
+                    Dictionary<string, object> map = new Dictionary<string, object>();
+                    map.Add("code", 0);
+                    map.Add("msg", "");
+                    map.Add("count", "");
+                    map.Add("data", list1);
+                    return Json(map, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    List<WH> list;
+                    if (value != null)
+                    {
+                        list = wMS.WH.Where(p => p.Name == value).ToList();
+                    }
+                    else
+                    {
+                        list = wMS.WH.ToList();
+                    }
+                    return Json(list,JsonRequestBehavior.AllowGet);
+                }
+            }
         }
-        #endregion
-        #region 员工登陆
-        public ActionResult login()
+    //查询库区信息
+    public ActionResult checkkq(string id, string value, Dictionary<string, string> data)
+    {
+            List<WH_Area> list;
+            List<checkKq_Result> list1;
+            using (WMSEntities wMS = new WMSEntities())
+            {
+
+                if (id == "1")
+                {
+                    StringBuilder builder = new StringBuilder("exec checkKq ");
+                    SqlParameter[] parameter = new SqlParameter[data.Count()];
+                    int index = 0;
+                    foreach (var da in data)
+                    {
+                        parameter[index] = new SqlParameter("@" + da.Key, da.Value);
+                        if ((index + 1) == data.Count())
+                        {
+                            builder.Append("@" + da.Key + "=@" + da.Key);
+                        }
+                        else
+                        {
+                            builder.Append("@" + da.Key + "=@" + da.Key + ",");
+                            index++;
+                        }
+                    }
+                    list1 = wMS.Database.SqlQuery<checkKq_Result>(builder.ToString(), parameter).ToList();
+                    Dictionary<string, object> map = new Dictionary<string, object>();
+                    map.Add("code", 0);
+                    map.Add("msg", "");
+                    map.Add("count", "");
+                    map.Add("data", list1);
+                    return Json(map, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    if (value != null)
+                    {
+                        list = wMS.WH_Area.Where(p => p.Name == value).ToList();
+                        return Json(list, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        list = wMS.WH_Area.ToList();
+                        return Json(list, JsonRequestBehavior.AllowGet);
+                    }
+
+                }
+            }
+        }
+    //查询库位信息
+    public ActionResult checkkw(string id, string value, Dictionary<string, string> data)
+    {
+            List<WH_StorageLocation> list;
+            List<checkkw_Result> list1;
+            using (WMSEntities wMS = new WMSEntities())
+            {
+
+                if (id == "1")
+                {
+                    StringBuilder builder = new StringBuilder("exec checkkw ");
+                    SqlParameter[] parameter = new SqlParameter[data.Count()];
+                    int index = 0;
+                    foreach (var da in data)
+                    {
+                        parameter[index] = new SqlParameter("@" + da.Key, da.Value);
+                        if ((index + 1) == data.Count())
+                        {
+                            builder.Append("@" + da.Key + "=@" + da.Key);
+                        }
+                        else
+                        {
+                            builder.Append("@" + da.Key + "=@" + da.Key + ",");
+                            index++;
+                        }
+                    }
+                    list1 = wMS.Database.SqlQuery<checkkw_Result>(builder.ToString(), parameter).ToList();
+                    Dictionary<string, object> map = new Dictionary<string, object>();
+                    map.Add("code", 0);
+                    map.Add("msg", "");
+                    map.Add("count", "");
+                    map.Add("data", list1);
+                    return Json(map, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    if (value != null)
+                    {
+                        list = wMS.WH_StorageLocation.Where(p => p.Name == value).ToList();
+                        return Json(list, JsonRequestBehavior.AllowGet);
+                    }
+                    else
+                    {
+                        list = wMS.WH_StorageLocation.ToList();
+                        return Json(list, JsonRequestBehavior.AllowGet);
+                    }
+
+                }
+            }
+        }
+    //查询货位信息
+    public ActionResult checkhuo(string id, string value, Dictionary<string, string> data)
+    {
+        List<WH_GoodsAllocation> list;
+        List<checkHuo_Result> list1;
+        using (WMSEntities wMS = new WMSEntities())
         {
-            return View();
+
+            if (id == "1")
+            {
+                StringBuilder builder = new StringBuilder("exec checkHuo ");
+                SqlParameter[] parameter = new SqlParameter[data.Count()];
+                int index = 0;
+                foreach (var da in data)
+                {
+                    parameter[index] = new SqlParameter("@" + da.Key, da.Value);
+                    if ((index + 1) == data.Count())
+                    {
+                        builder.Append("@" + da.Key + "=@" + da.Key);
+                    }
+                    else
+                    {
+                        builder.Append("@" + da.Key + "=@" + da.Key + ",");
+                        index++;
+                    }
+                }
+                list1 = wMS.Database.SqlQuery<checkHuo_Result>(builder.ToString(), parameter).ToList();
+                Dictionary<string, object> map = new Dictionary<string, object>();
+                map.Add("code", 0);
+                map.Add("msg", "");
+                map.Add("count", "");
+                map.Add("data", list1);
+                return Json(map, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                if (value != null)
+                {
+                    list = wMS.WH_GoodsAllocation.Where(p => p.Name == value).ToList();
+                    return Json(list, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    list = wMS.WH_GoodsAllocation.ToList();
+                    return Json(list, JsonRequestBehavior.AllowGet);
+                }
+
+            }
         }
-        #endregion
     }
+    //删除单个
+    public string DelSingle(string id, string type)
+    {
+        using (WMSEntities wMS = new WMSEntities())
+        {
+            int i = int.Parse(id);
+            if (type == "huo")
+            {
+                WH_GoodsAllocation wga = wMS.WH_GoodsAllocation.Where(c => c.ID == i).First();
+                wMS.WH_GoodsAllocation.Remove(wga);
+            }
+            if (type == "kq")
+            {
+                WH_Area wga = wMS.WH_Area.Where(c => c.ID == i).First();
+                wMS.WH_Area.Remove(wga);
+            }
+            if (type == "ca")
+            {
+                WH wga = wMS.WH.Where(c => c.ID == i).First();
+                wMS.WH.Remove(wga);
+            }
+            if (type == "kw")
+            {
+                WH_StorageLocation wga = wMS.WH_StorageLocation.Where(c => c.ID == i).First();
+                wMS.WH_StorageLocation.Remove(wga);
+            }
+            int w = wMS.SaveChanges();
+            if (w > 0)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+        }
+
+    }
+    //删除多个
+    public string DelAll(List<string> id, string type)
+    {
+        StringBuilder builder = new StringBuilder();
+        using (WMSEntities wMS = new WMSEntities())
+        {
+                int i = 0;
+            foreach (var all in id)
+                {
+                    if ((i + 1) == id.Count())
+                    {
+                        builder.Append(all);
+                    }
+                    else {
+                builder.Append(all+",");
+                    }
+                    i++;
+            }
+            SqlParameter[] parameter = new SqlParameter[2];
+            parameter[0] = new SqlParameter("@type", type);
+            parameter[1] = new SqlParameter("@id", builder.ToString());
+            wMS.Database.ExecuteSqlCommand("exec DelAll @type=@type,@id=@id", parameter);
+        }
+        return "true";
+    }
+    //修改操作
+    public string UpAll(Dictionary<string, string> data, string type)
+    {
+        using (WMSEntities wMS = new WMSEntities())
+        {
+            StringBuilder builder = new StringBuilder("exec UpAll @type=" + type + ",");
+            SqlParameter[] parameters = new SqlParameter[data.Count()];
+            int i = 0;
+            foreach (var da in data)
+            {
+                parameters[i] = new SqlParameter("@" + da.Key, da.Value);
+                if ((i + 1) == data.Count())
+                {
+                    builder.Append("@" + da.Key + "=@" + da.Key);
+                }
+                else
+                {
+                    builder.Append("@" + da.Key + "=@" + da.Key + ",");
+                }
+                i++;
+            }
+            wMS.Database.ExecuteSqlCommand(builder.ToString(), parameters);
+        }
+        return "true";
+    }
+    #endregion
+    #region 物料信息
+    public ActionResult Material()
+    {
+        return View();
+    }
+    #endregion
+    #region 库存限制维护
+    public ActionResult Inventory()
+    {
+        return View();
+    }
+    #endregion
+    #region 策略
+    public ActionResult Strategy()
+    {
+        return View();
+    }
+    #endregion
+    #region 员工登陆
+    public ActionResult login()
+    {
+        return View();
+    }
+    #endregion
+}
 }
